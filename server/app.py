8dfe86c4d0edf1c6aaacc0d796a938ba838c7fd8
WojMax <w.maksim@o2.pl>
2022-05-19 21:05:23 +0200

    API:server_and_database_connection_init
    'API:server_and_database_connection_init'

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+import psycopg2
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Calorie app server'
+
+
+def get_db_connection():
+    connection = psycopg2.connect(host='localhost',
+                                  database='flask_db',
+                                  user=os.environ['DB_USERNAME'],
+                                  password=os.environ['DB_PASSWORD'])
+    return connection
 
 
 if __name__ == '__main__':
     app.run()
+
+db_connection = get_db_connection()
+
+cursor = db_connection.cursor()
+
+cursor.close()
+db_connection.close()
