e7606291df89e454136d44418ae33cb4a64cf3ca
Krystyn <krystynmazurek@gmail.com>
2022-11-29 20:39:39 +0100

    APP: Profile

---
+++ mobile/components/Profile/Account.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+import { Text, View } from "../Themed";
+import t from "../../services/translations";
+import Colors from "../../constants/Colors";
+import useColorScheme from "../../hooks/useColorScheme";
+import { View as DefaultView } from "react-native";
+import { Auth } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { Entypo } from "@expo/vector-icons";
+
+export default function Account() {
+  const colorScheme = useColorScheme();
+  const [username, setusername] = useState("");
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const getUserInfo = async () => {
+    const userName = await Auth.currentUserInfo();
+    if (userName.attributes.email) setusername(userName.attributes.email);
+    return;
+  };
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: Colors[colorScheme].surface,
+        },
+      ]}
+    >
+      <DefaultView style={styles.account}>
+        <Text
+          style={[styles.dataText, { color: Colors[colorScheme].textDark }]}
+        >
+          Account
+        </Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={20}
+          color={Colors[colorScheme].textLight}
+          style={styles.icon}
+        />
+      </DefaultView>
+      <DefaultView style={styles.data}>
+        <DefaultView style={styles.avatar}></DefaultView>
+        <Text style={styles.dataText}> {username} </Text>
+      </DefaultView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    padding: 10,
+    borderRadius: 6,
+  },
+  account: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  data: { flex: 6 },
+  avatar: {
+    margin: 6,
+    width: 48,
+    height: 48,
+    backgroundColor: Colors.dark.topSurface,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  dataText: {
+    fontSize: 14,
+  },
+  icon: {
+    paddingRight: 2,
+  },
+});
---
+++ mobile/components/Profile/Weight.tsx
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+import { Text, View } from "../Themed";
+import t from "../../services/translations";
+import Colors from "../../constants/Colors";
+import useColorScheme from "../../hooks/useColorScheme";
+import { View as DefaultView } from "react-native";
+import { Auth } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { Entypo } from "@expo/vector-icons";
+
+export default function Weight() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: Colors[colorScheme].surface,
+        },
+      ]}
+    >
+      <DefaultView style={styles.account}>
+        <Text
+          style={[styles.dataText, { color: Colors[colorScheme].textDark }]}
+        >
+          Weight
+        </Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={20}
+          color={Colors[colorScheme].textLight}
+          style={styles.icon}
+        />
+      </DefaultView>
+      <DefaultView style={styles.data}></DefaultView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 6,
+  },
+  account: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  data: { flex: 5 },
+  avatar: {
+    margin: 4,
+    width: 48,
+    height: 48,
+    backgroundColor: Colors.dark.topSurface,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  dataText: {
+    fontSize: 14,
+  },
+  icon: {
+    paddingRight: 2,
+  },
+});
