b3279490c392c58a3c7d67e9c295d2cdda17787d
WojMax <w.maksim@o2.pl>
2023-01-23 19:05:58 +0100

    S:ml-models-updated-api

--- prediction_server/Functions/food_category_ml_functions.py
+++ prediction_server/Functions/food_category_ml_functions.py
@@ -1,39 +1,61 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import Model
-from keras.layers import Dense, Flatten, Dropout
-from keras.applications.inception_v3 import InceptionV3
+from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
+
 import numpy as np
+import os
+import random
+import scipy
+
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.preprocessing.image import ImageDataGenerator
 
 SHAPE = (299, 299, 3)
 
 
+def set_seed(seed):
+    tf.random.set_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
 def get_model(model_path):
-    base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=SHAPE)
+    base_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=SHAPE)
+
+    base_model.trainable = True
 
     x = base_model.output
-    x = Flatten()(x)
-    x = Dense(1024, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
     predictions = Dense(10, activation='softmax')(x)
 
     model = Model(inputs=base_model.input, outputs=predictions)
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.0001, momentum=0.9), loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-5), loss='categorical_crossentropy',
+                  metrics='accuracy')
 
     model.load_weights(model_path)
 
     return model
 
 
-def give_prediction(picture, model):
+def give_prediction(model):
+    directory = 'Predict'
+    
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_datagen.flow_from_directory(
+        directory,
+        target_size=(SHAPE[0], SHAPE[1]),
+        class_mode=None,
+        shuffle=False,
+        batch_size=1)  # set as testing data
+
     categories = ["bruschetta", "cappelletti", "gnocchi", "lasagne", "panna_cotta", "pizza", "ravioli", "risotto",
                   "spaghetti", "tiramisu"]
+    calories = [173, 307, 170, 192, 157, 266, 217, 226, 143, 335]
 
-    img = tf.keras.utils.load_img(picture, target_size=(SHAPE[0], SHAPE[1]))
-    x = tf.keras.utils.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    prediction = model.predict(images, batch_size=10)
+    predict = model.predict(test_generator, steps=1)
 
-    return categories[np.argmax(prediction[0])]
+    return categories[np.argmax(predict[0])], calories[np.argmax(predict[0])]
--- prediction_server/app.py
+++ prediction_server/app.py
@@ -2,22 +2,22 @@ import json
 import base64
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 
 import Functions.is_food_ml_functions as is_food
 import Functions.food_category_ml_functions as food_category
 
-application = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
 is_food_model_path = 'Models/OCC_Dish17_weights.h5'
-category_model_path = 'Models/dish_recognition39.h5'
+category_model_path = 'Models/dish_recognition29.h5'
 
 is_food_ml_model = is_food.get_model(is_food_model_path)
 category_ml_model = food_category.get_model(category_model_path)
 
 
-@application.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def parse_request():
     if request.method == 'POST':
@@ -26,20 +26,21 @@ def parse_request():
         # b64_pic = pic_json['assets'][0]['base64']
         b64_pic = pic_json['base64']
         encoded_pic_data = base64.b64decode(b64_pic)
-        filename = 'test_image.jpg'
+        filename = 'Predict/Data/test-image.jpg'
         with open(filename, 'wb') as f:
             f.write(encoded_pic_data)
 
         category = None
+        calories = None
         is_dish = is_food.give_prediction(filename, is_food_ml_model)
 
         if is_dish == 'Dish':
-            category = food_category.give_prediction(filename, category_ml_model)
+            category, calories = food_category.give_prediction(category_ml_model)
 
         os.remove(filename)
 
-        return {"category": category}
+        return jsonify({"category": category, "calories": calories})
 
 
 if __name__ == '__main__':
-    application.run(debug=True, host='127.0.0.1', port=5000)
+    app.run()
